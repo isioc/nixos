6c23811e3025135e0d08f014d26958e7110e6ab5
c <alternativa.c6986@mail.ru>
2026-01-23 19:10:55 +0300

    backup

--- backup-script.sh
+++ backup-script.sh
@@ -1,8 +1,9 @@
 cp -f /etc/nixos/configuration.nix $HOME/backup/nixos # nixos
 cp -f ~/.config/hypr/hyprland.conf $HOME/backup/configs # hyprland
 cp -f ~/.config/libinput-gestures.conf $HOME/backup/configs # touchpad
-cp -f ~/.config/rofi/config.rasi $HOME/backup/configs # rofi menu
- 
+cp -f ~/.config/wofi/config $HOME/backup/configs # wofi conf
+cp -f ~/.config/wofi/style.css $HOME/backup/configs # wofi style conf
+
 cd $HOME/backup
 git add *
 git commit -m "backup"
